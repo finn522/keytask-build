d7104ebd1e7d78875df67231040eacc8d9a2fea9
Simon de Bakker <simon@keytoe.nl>
2020-06-16 17:11:31 +0200

    transferred files

---
+++ precache-manifest.905363b6987d71e86f6fc506bc394071.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2d1aece8faf4b173b133e012692dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d1160876d110fcec7f7",
+    "url": "/static/css/2.bb342caa.chunk.css"
+  },
+  {
+    "revision": "79178d10ce89212425db",
+    "url": "/static/css/main.8f53889e.chunk.css"
+  },
+  {
+    "revision": "6d1160876d110fcec7f7",
+    "url": "/static/js/2.efd62cb3.chunk.js"
+  },
+  {
+    "revision": "ba4624d0d4869b81b34cf55ca014ac78",
+    "url": "/static/js/2.efd62cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79178d10ce89212425db",
+    "url": "/static/js/main.908707a4.chunk.js"
+  },
+  {
+    "revision": "0d14657c12f74d4b8b1f",
+    "url": "/static/js/runtime-main.8b125e90.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
